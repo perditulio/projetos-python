b75a305b2005fd6f5dcbd8a4b34f4893107d1b97
Tulio Perdigao <tuliopc2004@gmail.com>
2023-05-23 21:22:10 -0300

    Novos desafios

--- pythontest/aula022.py
+++ pythontest/aula022.py
@@ -1,11 +1,6 @@
-def fatorial(num=1):
-    f = 1
-    for c in range(num, 0, -1):
-        f *= c
-    return f
+from uteis import numeros
 
-
-f1 = fatorial(5)
-f2 = fatorial(4)
-f3 = fatorial()
-print(f'Os resultados são {f1}, {f2} e {f3}')
+num = int(input('Digite um número: '))
+fat = numeros.fatorial(num)
+print(f'O fatorial de {num} é {fat}.')
+print(f'O dobro de {num} é {numeros.dobro(num)}')
